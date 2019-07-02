cfcb9c8e767b36fd0cfc0aafd814d1f4d5a1b01b
Kesley de Mello <kesley_ksl@hotmail.com>
2019-07-02 10:15:07 -0300

    Geração de crud com scaffolding

---
+++ SiteAprendendo/Data/SiteAprendendoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SiteAprendendo.Models
+{
+    public class SiteAprendendoContext : DbContext
+    {
+        public SiteAprendendoContext (DbContextOptions<SiteAprendendoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SiteAprendendo.Models.Departament> Departament { get; set; }
+    }
+}
--- SiteAprendendo/Startup.cs
+++ SiteAprendendo/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SiteAprendendo.Models;
 
 namespace SiteAprendendo
 {
@@ -33,6 +35,9 @@ namespace SiteAprendendo
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<SiteAprendendoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SiteAprendendoContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
