9999c238782927982ca701bb0dc27b4c6a5db1b2
Kesley de Mello <kesley_ksl@hotmail.com>
2019-07-03 10:38:49 -0300

    Other entities and second migration

--- SiteAprendendo/Data/SiteAprendendoContext.cs
+++ SiteAprendendo/Data/SiteAprendendoContext.cs
@@ -13,6 +13,9 @@ namespace SiteAprendendo.Models
         {
         }
 
-        public DbSet<SiteAprendendo.Models.Departament> Departament { get; set; }
+        public DbSet<Departament> Departament { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+       
     }
 }
--- SiteAprendendo/Migrations/20190703132145_OtherEntities.Designer.cs
+++ SiteAprendendo/Migrations/20190703132145_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SiteAprendendo.Models;
 
 namespace SiteAprendendo.Migrations
 {
     [DbContext(typeof(SiteAprendendoContext))]
-    partial class SiteAprendendoContextModelSnapshot : ModelSnapshot
+    [Migration("20190703132145_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
