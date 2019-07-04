5186078f9d8facd4c5ec70f6d8bfbeb952c594eb
Kesley de Mello <kesley_ksl@hotmail.com>
2019-07-04 17:22:32 -0300

    App locale, number and date formating

--- SiteAprendendo/Startup.cs
+++ SiteAprendendo/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.EntityFrameworkCore;
 using SiteAprendendo.Models;
 using SiteAprendendo.Data;
 using SiteAprendendo.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace SiteAprendendo
 {
@@ -48,6 +50,14 @@ namespace SiteAprendendo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+            app.UseRequestLocalization(localizationOptions);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
