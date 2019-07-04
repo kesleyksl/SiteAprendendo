c07b2f1a686a8b67205ea0bbeccdc655710fc3ed
Kesley de Mello <kesley_ksl@hotmail.com>
2019-07-04 16:04:47 -0300

    Update seller and custom service exception

--- SiteAprendendo/Controllers/SellersController.cs
+++ SiteAprendendo/Controllers/SellersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using SiteAprendendo.Models;
 using SiteAprendendo.Services;
 using SiteAprendendo.Models.ViewModels;
-
+using SiteAprendendo.Services.Exceptions;
 
 namespace SiteAprendendo.Controllers
 {
@@ -73,6 +73,46 @@ namespace SiteAprendendo.Controllers
             }
             return View(obj);
 
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Departament> departaments = _departamentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departaments = departaments };
+            return View(viewModel);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
---
+++ SiteAprendendo/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SiteAprendendo.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
